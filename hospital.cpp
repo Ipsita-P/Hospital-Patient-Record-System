53bd9981058c55b6530c079caa10833453bda02f
Ipsita Puruseth <ipsitapurusth03@gmail.com>
2026-02-11 15:12:58 +0530

    Added update/delete feature

--- hospital.cpp
+++ hospital.cpp
@@ -33,7 +33,6 @@ public:
 };
 
 
-
 // ➤ ADD RECORD
 void addRecord() {
     Patient p;
@@ -47,25 +46,21 @@ void addRecord() {
 }
 
 
-
 // ➤ VIEW RECORDS
 void viewRecords() {
     Patient p;
     ifstream file("patients.dat", ios::binary);
 
-    cout << "\n--- Patient Records ---\n";
-
-    while (file.read((char*)&p, sizeof(p))) {
+    while(file.read((char*)&p, sizeof(p))) {
         p.display();
-        cout << "-----------------------";
+        cout << "-------------------";
     }
 
     file.close();
 }
 
 
-
-// ➤ SEARCH RECORD (New Feature)
+// ➤ SEARCH RECORD
 void searchRecord() {
     int searchID;
     bool found = false;
@@ -76,8 +71,8 @@ void searchRecord() {
     Patient p;
     ifstream file("patients.dat", ios::binary);
 
-    while (file.read((char*)&p, sizeof(p))) {
-        if (p.id == searchID) {
+    while(file.read((char*)&p, sizeof(p))) {
+        if(p.id == searchID) {
             cout << "\nRecord Found:\n";
             p.display();
             found = true;
@@ -85,13 +80,76 @@ void searchRecord() {
         }
     }
 
-    if (!found) {
+    if(!found)
         cout << "Record Not Found!\n";
-    }
 
     file.close();
 }
 
+
+// ➤ DELETE RECORD
+void deleteRecord() {
+    int deleteID;
+    bool found = false;
+
+    cout << "Enter Patient ID to Delete: ";
+    cin >> deleteID;
+
+    Patient p;
+    ifstream file("patients.dat", ios::binary);
+    ofstream temp("temp.dat", ios::binary);
+
+    while(file.read((char*)&p, sizeof(p))) {
+        if(p.id != deleteID)
+            temp.write((char*)&p, sizeof(p));
+        else
+            found = true;
+    }
+
+    file.close();
+    temp.close();
+
+    remove("patients.dat");
+    rename("temp.dat", "patients.dat");
+
+    if(found)
+        cout << "Record Deleted!\n";
+    else
+        cout << "Record Not Found!\n";
+}
+
+
+// ➤ UPDATE RECORD
+void updateRecord() {
+    int updateID;
+    bool found = false;
+
+    cout << "Enter Patient ID to Update: ";
+    cin >> updateID;
+
+    fstream file("patients.dat", ios::binary | ios::in | ios::out);
+    Patient p;
+
+    while(file.read((char*)&p, sizeof(p))) {
+        if(p.id == updateID) {
+            cout << "Enter New Details:\n";
+            p.input();
+
+            file.seekp(-sizeof(p), ios::cur);
+            file.write((char*)&p, sizeof(p));
+
+            found = true;
+            break;
+        }
+    }
+
+    if(found)
+        cout << "Record Updated!\n";
+    else
+        cout << "Record Not Found!\n";
+
+    file.close();
+}
 
 
 // ➤ MAIN MENU
@@ -103,19 +161,23 @@ int main() {
         cout << "1. Add Record\n";
         cout << "2. View Records\n";
         cout << "3. Search Record\n";
-        cout << "4. Exit\n";
+        cout << "4. Update Record\n";
+        cout << "5. Delete Record\n";
+        cout << "6. Exit\n";
         cout << "Enter Choice: ";
         cin >> choice;
 
-        switch (choice) {
+        switch(choice) {
             case 1: addRecord(); break;
             case 2: viewRecords(); break;
             case 3: searchRecord(); break;
-            case 4: cout << "Exiting...\n"; break;
+            case 4: updateRecord(); break;
+            case 5: deleteRecord(); break;
+            case 6: cout << "Exiting...\n"; break;
             default: cout << "Invalid Choice!\n";
         }
 
-    } while (choice != 4);
+    } while(choice != 6);
 
     return 0;
 }
